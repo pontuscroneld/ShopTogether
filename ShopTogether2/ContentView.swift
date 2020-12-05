a1c14ed57b42b49cc6dfecd8cd2ffa06aee3b22c
pontuscroneld <pontuscroneld@gmail.com>
2020-12-05 16:51:08 +0100

    Initial Commit

---
+++ ShopTogether2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShopTogether2
+//
+//  Created by Pontus Croneld on 2020-12-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShopTogether2/ShopTogether2App.swift
@@ -0,0 +1,17 @@
+//
+//  ShopTogether2App.swift
+//  ShopTogether2
+//
+//  Created by Pontus Croneld on 2020-12-05.
+//
+
+import SwiftUI
+
+@main
+struct ShopTogether2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
